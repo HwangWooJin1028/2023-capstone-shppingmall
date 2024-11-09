62141a81c2327e1e7397456300efd994a1d5ca1e
HwangWooJin1028 <95200625+HwangWooJin1028@users.noreply.github.com>
2024-11-09 14:57:31 +0900

    Update ChatCompletion.java

--- BelleafShoppingMall/src/main/java/com/shoppingmall/belleaf/chatting/vo/ChatCompletion.java
+++ BelleafShoppingMall/src/main/java/com/shoppingmall/belleaf/chatting/vo/ChatCompletion.java
@@ -3,8 +3,9 @@ package com.shoppingmall.belleaf.chatting.vo;
 import java.util.List;
 
 
+// 2024-11-09 수정 - pasing 내용 변경
 public class ChatCompletion {
-	public String id;
+    public String id;
     public String object;
     public long created;
     public String model;
@@ -22,11 +23,27 @@ public class ChatCompletion {
     public static class Message {
         public String role;
         public String content;
+        public Object refusal; // 추가된 필드
     }
 
     public static class Usage {
         public int prompt_tokens;
         public int completion_tokens;
         public int total_tokens;
+        public PromptTokensDetails prompt_tokens_details; // 추가된 필드
+        public CompletionTokensDetails completion_tokens_details; // 추가된 필드
     }
+
+    public static class PromptTokensDetails {
+        public int cached_tokens;
+        public int audio_tokens;
+    }
+
+    public static class CompletionTokensDetails {
+        public int reasoning_tokens;
+        public int audio_tokens;
+        public int accepted_prediction_tokens;
+        public int rejected_prediction_tokens;
+    } 
 }
+
